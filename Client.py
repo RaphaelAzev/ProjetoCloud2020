955d8f331cf0b09e0ec3db5ad2e62e1193283b61
raphaelazev <raphaelazev@hotmail.com>
2020-12-02 05:17:47 -0300

    Barebones client get e post

---
+++ Client.py
@@ -0,0 +1,25 @@
+import json
+import requests
+import datetime
+import sys
+
+with open('LoadDNS.txt', 'r') as arq:
+    my_url = arq.readline()
+
+#print(url)
+
+if sys.argv[1] == 'getAll':
+    r = requests.get(url=my_url + "/alltasks")
+    print(json.dumps(r.json())
+
+if sys.argv[1] == 'postTask':
+    date = str(datetime.datetime.now())
+    title = input('Title da task: ')
+    description = input('Descricao da task')
+    jsonfile = {
+        "title": title,
+        "pub_date": date,
+        "description": description
+    }
+    r = requests.post(url=my_url + "/posttask",json=jsonfile)
+    
