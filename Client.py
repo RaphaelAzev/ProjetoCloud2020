fdb32ef4cab731b800e0e932d71b94785c5204d1
raphaelazev <raphaelazev@hotmail.com>
2020-12-02 06:17:42 -0300

    Bug no client consertado e client testado e funcionando

--- Client.py
+++ Client.py
@@ -6,16 +6,18 @@ import sys
 with open('LoadDNS.txt', 'r') as arq:
     my_url = arq.readline()
 
+#my_url = 'http://3.88.142.177:8080/tasks'
+
 #print(url)
 
 if sys.argv[1] == 'getAll':
     r = requests.get(url=my_url + "/alltasks")
-    print(json.dumps(r.json())
+    print(json.dumps(r.json()))
 
 if sys.argv[1] == 'postTask':
     date = str(datetime.datetime.now())
     title = input('Title da task: ')
-    description = input('Descricao da task')
+    description = input('Descricao da task: ')
     jsonfile = {
         "title": title,
         "pub_date": date,
